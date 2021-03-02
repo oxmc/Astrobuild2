71080a72e1762ea5a149774a5d9fea83ced00799
oxmc <67136658+oxmc@users.noreply.github.com>
2021-03-02 02:15:49 -0800

    Add files via upload

---
+++ js/pop-up.js
@@ -0,0 +1,11 @@
+function OpenPopUp(){
+    document.getElementById("open-popup-btn").addEventListener("click",function(){
+    document.getElementsByClassName("popup")[0].classList.add("active");
+});
+}
+
+function ClosePopUp(){
+    document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
+    document.getElementsByClassName("popup")[0].classList.remove("active");
+});
+}
